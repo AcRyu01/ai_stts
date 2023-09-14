db3e4cd0acb78fb488e2067303eac1597cc0adf9
krajorn <64050636@kmitl.ac.th>
2023-09-14 23:09:04 +0700

    first mock eiei

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,47 @@
 // import Image from "next/image";
 
 export default function Home() {
-  return <div className="p-10">STTS</div>;
+  return (
+    <>
+      <div className="w-auto h-[100px] bg-orange-400 shadow flex">
+        <p className="w-[247px] h-[59px] text-black text-[64px] font-normal font-['Sofia Sans'] ml-[40px]">
+          STTTS
+        </p>
+        <p className="w-[433px] h-[115px] text-black text-[40px] font-normal font-['Sofia Sans'] mx-auto mt-[23px]">
+          make your life ez
+        </p>
+      </div>
+      <div className="flex justify-center gap-[87px] mt-[81px]">
+        <div className="w-[220px] h-[70px] bg-white rounded-[50px] border-2 border-orange-500">
+          <p className="w-[78.32px] h-[34.12px] text-center text-orange-500 text-[40px] font-bold font-['Sofia Sans'] ml-[71px]">
+            ไทย
+          </p>
+        </div>
+        <div>eiei</div>
+        <div className="w-[220px] h-[70px] bg-white rounded-[50px] border-2 border-orange-500">
+          <p className="w-[78.32px] h-[34.12px] text-center text-orange-500 text-[40px] font-bold font-['Sofia Sans'] ml-[40px]">
+            English
+          </p>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="w-[538px] h-[625px] bg-orange-50 rounded-[50px] ml-[148px] mt-[49px] pt-[28px]">
+          <div className="w-[170px] h-10 bg-orange-500 rounded-[50px] py-[8px] ml-[41px]">
+            <p className="ml-[25px]  text-white text-xl font-bold font-['Sofia Sans']">
+              Uplode flies
+            </p>
+          </div>
+          <div className="mt-[300px] mx-[212px]">
+            <p className="w-[132px] h-[41px] text-black text-xl font-normal font-['Sofia Sans']">
+              chose file
+            </p>
+          </div>
+        </div>
+        <div className="w-[538px] h-[1101px] bg-orange-50 rounded-[50px] mt-[49px] ml-[52px] pt-[27px]">
+          <div className="w-[50px] h-[50px] bg-orange-500 rounded-[50px] ml-[45px]"></div>
+          <p className="ml-[70px] mt-[61px]"> eieieieiieieieieieieieiie </p>
+        </div>
+      </div>
+    </>
+  );
 }
