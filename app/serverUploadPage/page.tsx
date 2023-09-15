44f06ba9fa2722ee324deffdc99b01bd22100ac8
Nrokitin <64050518@kmitl.ac.th>
2023-09-15 15:34:34 +0700

    feat(play): play mp3 file from file path

--- app/serverUploadPage/page.tsx
+++ app/serverUploadPage/page.tsx
@@ -15,20 +15,41 @@ export default function Page() {
 
     // With the file data in the buffer, you can do whatever you want with it.
     // For this, we'll just write it to the filesystem in a new location
-    const path = join("/", "tmp", file.name);
+    const path = join("./", "tmp", file.name);
     await writeFile(path, buffer);
     console.log(`open ${path} to see the uploaded file`);
 
     return { success: true };
   }
 
+  async function log() {
+    "use server";
+    const path = ("./tmp/Friend! Hello friend!.mp3")
+    console.log(`open ${path} to see the uploaded file`);
+  }
+
+
   return (
     <main>
       <h1>React Server Component: Upload</h1>
       <form action={upload}>
-        <input type="file" name="file" />
+        <input type="file" accept=".mp3" name="file" />
         <input type="submit" value="Upload" />
       </form>
+
+
+      <h1>React Server Component: TEST</h1>
+      <div>
+        <h1>Audio Player</h1>
+        <audio controls>
+          <source src="/audio/Friend! Hello friend!.mp3" type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+
+
+
+
     </main>
   );
 }
