36282d6c271ef7e904b8c5357eee5d5407e5acf5
Nrokitin <64050518@kmitl.ac.th>
2023-09-21 22:52:50 +0700

    feat(upload):reject wrong file format

--- app/serverUploadPage/page.tsx
+++ app/serverUploadPage/page.tsx
@@ -6,20 +6,22 @@ export default function Page() {
     "use server";
 
     const file: File | null = data.get("file") as unknown as File;
-    if (!file) {
-      throw new Error("No file uploaded");
+    const fileExtension = file.name.split(".").pop();
+
+    if (fileExtension === "mp3") {
+      const bytes = await file.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+
+      // With the file data in the buffer, you can do whatever you want with it.
+      // For this, we'll just write it to the filesystem in a new location
+      const path = join("./tmp", file.name);
+      await writeFile(path, buffer);
+      console.log("open ${path} to see the uploaded file");
+      return { success: true, filePath: path }; // Return the file path
+    } else {
+      console.log("Invalid file format. Please upload an MP3 file.");
+      return { success: false };
     }
-
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-
-    // With the file data in the buffer, you can do whatever you want with it.
-    // For this, we'll just write it to the filesystem in a new location
-    const path = join("./", "tmp", file.name);
-    await writeFile(path, buffer);
-    console.log(`open ${path} to see the uploaded file`);
-
-    return { success: true };
   }
 
   return (
