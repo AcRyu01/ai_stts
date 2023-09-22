5bfedf5c44f9b9f96ec9b4a62e1c847fb8e71cc9
64050502 <64050502@kmitl.ac.th>
2023-09-22 22:54:51 +0700

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,37 @@
 // import Image from "next/image";
+"use client";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [currentLanguage, setCurrentLanguage] = useState("th"); // Default language
+
+  const handleLanguageSwap = () => {
+    // Function to toggle between Thai ("th") and English ("en")
+    setCurrentLanguage(currentLanguage === "th" ? "en" : "th");
+  };
+
+  const handleFileUpload = async () => {
+    const formData = new FormData();
+    formData.append("file", selectedFile); // selectedFile is the file input field value
+  
+    try {
+      const response = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log("File uploaded successfully. File path:", data.filePath);
+      } else {
+        const errorData = await response.json();
+        console.error("File upload failed. Error:", errorData.error);
+      }
+    } catch (error) {
+      console.error("An error occurred while uploading the file:", error);
+    }
+  };
+
   return (
     <>
       <div className="w-auto h-[100px] bg-orange-400 shadow flex">
@@ -11,31 +42,42 @@ export default function Home() {
           make your life ez
         </p>
       </div>
+      
       <div className="flex justify-center gap-[87px] mt-[81px]">
         <div className="w-[220px] h-[70px] bg-white rounded-[50px] border-2 border-orange-500">
           <p className="w-[78.32px] h-[34.12px] text-center text-orange-500 text-[40px] font-bold font-['Sofia Sans'] ml-[71px]">
-            ไทย
+          {currentLanguage === "th" ? "ไทย" : "English"}
           </p>
         </div>
-        <div>eiei</div>
+        <div className="flex justify-center gap-[87px] mt-[81px]">
+          <div
+            className="w-[220px] h-[70px] bg-white rounded-[50px] border-2 border-orange-500 cursor-pointer"
+            onClick={handleLanguageSwap}
+          >
+          </div>
+          <div>eiei</div>
+        </div>
         <div className="w-[220px] h-[70px] bg-white rounded-[50px] border-2 border-orange-500">
           <p className="w-[78.32px] h-[34.12px] text-center text-orange-500 text-[40px] font-bold font-['Sofia Sans'] ml-[40px]">
-            English
+          {currentLanguage === "th" ? "English" : "ไทย"}
           </p>
         </div>
       </div>
+
       <div className="flex">
         <div className="w-[538px] h-[625px] bg-orange-50 rounded-[50px] ml-[148px] mt-[49px] pt-[28px]">
-          <div className="w-[170px] h-10 bg-orange-500 rounded-[50px] py-[8px] ml-[41px]">
-            <p className="ml-[25px]  text-white text-xl font-bold font-['Sofia Sans']">
-              Uplode flies
-            </p>
-          </div>
-          <div className="mt-[300px] mx-[212px]">
-            <p className="w-[132px] h-[41px] text-black text-xl font-normal font-['Sofia Sans']">
-              chose file
-            </p>
-          </div>
+          <form action={handleFileUpload}>
+            <div className="w-[170px] h-10 bg-orange-500 rounded-[50px] py-[8px] ml-[41px]">
+              <input
+                type="submit"
+                value="Upload files"
+                className="ml-[25px]  text-white text-xl font-bold font-['Sofia Sans']"
+              />
+            </div>
+            <div className="mt-[300px] mx-[195px]">
+              <input type="file" accept=".mp3" name="file" />
+            </div>
+          </form>
         </div>
         <div className="w-[538px] h-[1101px] bg-orange-50 rounded-[50px] mt-[49px] ml-[52px] pt-[27px]">
           <div className="w-[50px] h-[50px] bg-orange-500 rounded-[50px] ml-[45px]"></div>
--- app/serverUploadPage/page.tsx
+++ app/serverUploadPage/page.tsx
@@ -1,10 +1,9 @@
 import { writeFile } from "fs/promises";
 import { join } from "path";
 
-export default function Page() {
-  async function upload(data: FormData) {
-    "use server";
-
+export default async function handler(data: FormData) {
+  // Function to handle file upload
+    // You can implement your file upload logic here
     const file: File | null = data.get("file") as unknown as File;
     const fileExtension = file.name.split(".").pop();
 
@@ -22,23 +21,4 @@ export default function Page() {
       console.log("Invalid file format. Please upload an MP3 file.");
       return { success: false };
     }
-  }
-
-  return (
-    <main>
-      <h1>React Server Component: Upload</h1>
-      <form action={upload}>
-        <input type="file" accept=".mp3" name="file" />
-        <input type="submit" value="Upload" />
-      </form>
-      <h1>React Server Component: Play</h1>
-      <div>
-        <h1>Audio Player</h1>
-        <audio controls>
-          <source src="/audio/6.mp3" type="audio/mpeg" />
-            Your browser does not support the audio element.
-        </audio>
-      </div>
-    </main>
-  );
 }
