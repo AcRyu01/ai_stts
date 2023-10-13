5e55ea247c3be2019f44627d9716f21115319a6e
krajorn <64050636@kmitl.ac.th>
2023-10-13 19:44:11 +0700

    main page 1440

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,115 @@
-// import Image from "next/image";
+import Image from "next/image";
+import Logo from "../app/pic/STTTS.png";
+import SwitchBut from "../app/pic/switch-horizontal.png";
+import Upload from "../app/pic/cloud-upload.png";
+import lock from "../app/pic/lock-open.png";
+import user from "../app/pic/user-group.png";
+import dup from "../app/pic/document-duplicate.png";
+import dowload from "../app/pic/download.png";
+import up from "../app/pic/up.png";
+import dow from "../app/pic/dow.png";
 
 export default function Home() {
   return (
     <>
-      <div className="w-auto h-[100px] bg-orange-400 shadow flex">
-        <p className="w-[247px] h-[59px] text-black text-[64px] font-normal font-['Sofia Sans'] ml-[40px]">
-          STTTS
-        </p>
-        <p className="w-[433px] h-[115px] text-black text-[40px] font-normal font-['Sofia Sans'] mx-auto mt-[23px]">
-          make your life ez
+      {/*header*/}
+      <div className="w-auto h-[80px] bg-orange-500 flex justify-between pr-9 ">
+        <Image src={Logo} alt="logo" width={250} height={75}></Image>
+        <p className="text-2xl font-['Salsa'] text-white pt-10">
+          “ Effortless translation starts with your upload. ”
         </p>
       </div>
-      <div className="flex justify-center gap-[87px] mt-[81px]">
-        <div className="w-[220px] h-[70px] bg-white rounded-[50px] border-2 border-orange-500">
-          <p className="w-[78.32px] h-[34.12px] text-center text-orange-500 text-[40px] font-bold font-['Sofia Sans'] ml-[71px]">
-            ไทย
-          </p>
+
+      {/*content*/}
+
+      {/*langeuage and Switch button*/}
+      <div className="flex mt-[69px]">
+        <div className="flex gap-x-[60px] text-[32px] font-semibold font-['Sofia Sans'] text-zinc-600 ml-[151px]">
+          <p>thai</p>
+          <p>english</p>
         </div>
-        <div>eiei</div>
-        <div className="w-[220px] h-[70px] bg-white rounded-[50px] border-2 border-orange-500">
-          <p className="w-[78.32px] h-[34.12px] text-center text-orange-500 text-[40px] font-bold font-['Sofia Sans'] ml-[40px]">
-            English
-          </p>
+        <button className="ml-[310px]">
+          <Image src={SwitchBut} alt="SwitchBut"></Image>
+        </button>
+        <div className="flex gap-x-[60px] text-[32px] font-semibold font-['Sofia Sans'] text-zinc-600 ml-[50px]">
+          <p>thai</p>
+          <p>english</p>
         </div>
       </div>
-      <div className="flex">
-        <div className="w-[538px] h-[625px] bg-orange-50 rounded-[50px] ml-[148px] mt-[49px] pt-[28px]">
-          <div className="w-[170px] h-10 bg-orange-500 rounded-[50px] py-[8px] ml-[41px]">
-            <p className="ml-[25px]  text-white text-xl font-bold font-['Sofia Sans']">
-              Uplode flies
-            </p>
-          </div>
-          <div className="mt-[300px] mx-[212px]">
-            <p className="w-[132px] h-[41px] text-black text-xl font-normal font-['Sofia Sans']">
-              chose file
-            </p>
-          </div>
+
+      {/*box box*/}
+      <div className="flex  gap-x-[36px] justify-center">
+        {/*gray box*/}
+        <div className="w-[633px] h-[625px] bg-neutral-200 bg-opacity-90 rounded-[20px]">
+          <button className="w-[170px] h-[40px] bg-orange-500 rounded-[50px] mt-[20px] ml-[25px] text-white">
+            Upload
+          </button>
+          <button className=" mt-[520px] ml-[568px]">
+            <Image src={dup} alt="dup"></Image>
+          </button>
         </div>
-        <div className="w-[538px] h-[1101px] bg-orange-50 rounded-[50px] mt-[49px] ml-[52px] pt-[27px]">
-          <div className="w-[50px] h-[50px] bg-orange-500 rounded-[50px] ml-[45px]"></div>
-          <p className="ml-[70px] mt-[61px]"> eieieieiieieieieieieieiie </p>
+
+        {/*orange box*/}
+        <div className="w-[633px] h-[625px] bg-orange-50 rounded-[20px]">
+          <button className=" mt-[580px] ml-[524px]">
+            <Image src={dowload} alt="dowload"></Image>
+          </button>
+          <button className="ml-[12px]">
+            <Image src={dup} alt="dup"></Image>
+          </button>
+        </div>
+      </div>
+
+      {/*rate*/}
+      <div className="flex ml-[779px] mt-[10px] gap-6">
+        <button>
+          <Image src={up} alt="up"></Image>
+        </button>
+        <button>
+          <Image src={dow} alt="dow"></Image>
+        </button>
+      </div>
+
+      {/*footer*/}
+      <div className="w-auto h-[535px] bg-orange-400 bg-opacity-60 mt-[731px] pt-[88px]">
+        <div className="ml-[174px]">
+          <div className="flex">
+            <Image src={Upload} alt="upload"></Image>
+            <div className="ml-[65px]">
+              <p className="text-[32px] font-bold font-['Sofia Sans']">
+                File Upload
+              </p>
+              <p>
+                Uploaded files must be in .mp3 format and must not exceed 30
+                seconds in length
+              </p>
+            </div>
+          </div>
+          <div className="mt-[85px] flex">
+            <Image src={lock} alt="lock"></Image>
+            <div className="ml-[65px]">
+              <p className="text-[32px] font-bold font-['Sofia Sans']">
+                Security
+              </p>
+              <p className="w-[886px]">
+                You may be confident that our translation tool won't keep your
+                files on our server longer than is necessary. When you exit the
+                website, your data and results will be removed from our server.
+              </p>
+            </div>
+          </div>
+          <div className="mt-[76px] flex">
+            <Image src={user} alt="user"></Image>
+            <div className="ml-[65px]">
+              <p className="text-[32px] font-bold font-['Sofia Sans']">
+                User-Friendly
+              </p>
+              <p>
+                Your audio files can be translated quickly and simply. You can
+                just upload your file without downloading any software.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
